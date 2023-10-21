2fc262908dd45cb73dfaadadda1ee8f1f0d3849c
Jannat Ara Ferdouse Raya <jannat.raya@northsouth.edu>
2023-10-21 22:54:41 +0600

    concept on hooks in react

--- counter_project/src/App.jsx
+++ counter_project/src/App.jsx
@@ -1,12 +1,32 @@
+import './App.css'
 import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(5)
+  //without using states, it will only update the js not the UI. With the help of hooks we can make changes in the UI.
+  const addValue = () => {
+    console.log("Before clicked:", counter);
+    setCounter(counter + 1);
 
+  }
+  console.log("After click on Add Value:", counter);
+  const removeValue = () => {
+    console.log("Before clicked:", counter);
+    setCounter(counter -1);
+
+  }
   return (
     <>
-     
+      <div>
+        <h2>Count Value:{counter}</h2>
+        <button
+          onClick={addValue}
+        >Add Value</button>
+        <br />
+        <button
+          onClick={removeValue}>remove Value</button>
+      </div>
     </>
   )
 }
