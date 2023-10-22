f25f1aa4efe6e8fe995c06945fc5c12e6538c168
Jannat Ara Ferdouse Raya <jannat.raya@northsouth.edu>
2023-10-22 21:43:16 +0600

    Tailwind and props

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './src/**/*.css', // Add your CSS file here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
